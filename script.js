3fe6e552e530e4e6a6fc2e3efe508f8a85341fc5
Lyba T <105508273+LybaaT@users.noreply.github.com>
2022-10-30 20:59:04 +0500

    removed php

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ document.body.onscroll = function() {
         }
     })
 };
+
+function ContactMe() {
+    let inputs = document.querySelectorAll('input, select, textarea')
+    
+    fetch(`https://laiba.warze.org/?formsubmission=yes&formfirstname=${inputs[0].value}&formlastname=${inputs[1].value}&formemail=${inputs[2].value}&formreason=${inputs[3].value}&formmessage=${inputs[4].value}`).then((r) => {
+        alert("Thank you for reaching out! Your message has been sent. I'll get back to you as soon as you can.");
+    }
+}
